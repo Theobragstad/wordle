6db178d2a36b073acc910bbe22c23d0e99e916ca
Theo Bragstad <theo.bragstad@colorado.edu>
2023-11-14 22:16:40 -0700

    adding

---
+++ ai_player.py
@@ -0,0 +1,9 @@
+class AIPlayer:
+    def __init__(self):
+        return None
+
+    def get_guessable_words(self):
+       return None
+    
+    def get_next_guess(self):
+        return "hello"
--- wordle.py
+++ wordle.py
@@ -1,6 +1,8 @@
 import random
-import random_player
 import time
+
+import random_player
+import ai_player
 
 
 class Wordle:
@@ -9,7 +11,7 @@ class Wordle:
         self.guessable = self.read_guessable()
         self.used_words = set()
         self.n_rounds = 6
-        self.responses = ['⬜️', '🟨', '🟩']
+        self.responses = ["⬜️", "🟨","🟩"]
         self.answer = None
         self.answer_char_counts = None
         self.all_responses = None
@@ -21,7 +23,7 @@ class Wordle:
     def read_guessable(self):
         with open("guessable.txt", "r") as file:
             return file.read().splitlines()
-        
+
     def get_random_word(self):
         available_words = list(set(self.answers) - self.used_words)
 
@@ -37,39 +39,39 @@ class Wordle:
     def play(self):
         self.all_responses = list()
         self.answer = self.get_random_word()
-        self.answer_char_counts = {char: self.answer.count(char) for char in self.answer}
+        self.answer_char_counts = {
+            char: self.answer.count(char) for char in self.answer
+        }
 
-        for i in range (1, self.n_rounds + 1):
+        for i in range(1, self.n_rounds + 1):
             print(f"\nRound {i}")
             while True:
                 user_guess = input("Guess: ")
                 if user_guess in self.answers or user_guess in self.guessable:
                     break
-            
+
             print(f"Response: {self.evaluate(user_guess)}")
             if user_guess == self.answer:
                 self.display_results()
                 return
-        
+
         print(f"The word was {self.answer}\n")
         self.display_results()
 
         self.check_play_again()
 
-
     def check_play_again(self):
         repeat = False
         while True:
             repeat_input = input("\nPlay again? y/n: ")
-            if repeat_input == 'n':
+            if repeat_input == "n":
                 return
-            elif repeat_input == 'y':
+            elif repeat_input == "y":
                 repeat = True
                 break
 
         if repeat:
             self.play()
-
 
     def evaluate(self, guess):
         response = list()
@@ -83,17 +85,21 @@ class Wordle:
                     response.append(self.responses[2])
                     guess_char_mark_counts[guess[i]] += 1
                 else:
-                    response.append(' ')
+                    response.append(" ")
 
             for i in range(len(guess)):
-                if response[i] == ' ':
-                    if guess[i] in self.answer and guess_char_mark_counts[guess[i]] < self.answer_char_counts[guess[i]]:
+                if response[i] == " ":
+                    if (
+                        guess[i] in self.answer
+                        and guess_char_mark_counts[guess[i]]
+                        < self.answer_char_counts[guess[i]]
+                    ):
                         response[i] = self.responses[1]
                         guess_char_mark_counts[guess[i]] += 1
                     else:
-                        response[i] = self.responses[0]    
+                        response[i] = self.responses[0]
 
-        response_string = ''.join(response)
+        response_string = "".join(response)
         self.all_responses.append(response_string)
         return response_string
 
@@ -104,7 +110,7 @@ class Wordle:
 
     def get_user_choice(self):
         print("\n⬜️🟨🟩 Wordle\n")
-        while True: 
+        while True:
             print("1. You play")
             print("2. Random player")
             print("3. AI player")
@@ -112,16 +118,17 @@ class Wordle:
             choice = int(input("Select a game mode: "))
             if 0 <= choice <= 3:
                 return choice
-                
 
     def play_random(self, show_full_output):
         rp = random_player.RandomPlayer()
 
         self.all_responses = list()
         self.answer = self.get_random_word()
-        self.answer_char_counts = {char: self.answer.count(char) for char in self.answer}
+        self.answer_char_counts = {
+            char: self.answer.count(char) for char in self.answer
+        }
 
-        for i in range (1, self.n_rounds + 1):
+        for i in range(1, self.n_rounds + 1):
             if show_full_output:
                 print(f"\nRound {i}")
             while True:
@@ -130,7 +137,7 @@ class Wordle:
                     print(f"Guess: {user_guess}")
                 if user_guess in self.answers or user_guess in self.guessable:
                     break
-            
+
             response = self.evaluate(user_guess)
 
             if show_full_output:
@@ -139,68 +146,146 @@ class Wordle:
                 self.display_results()
                 time.sleep(1.5)
                 return i
-        
+
         if show_full_output:
             print(f"The word was {self.answer}\n")
-            
+
         self.display_results()
 
         return 0
-        
+
     def start(self):
         mode = self.get_user_choice()
-        if(mode == 0):
+        if mode == 0:
             return
-        elif(mode == 1):
+        elif mode == 1:
             self.play()
-        elif(mode == 2):
-            simulation_count = self.get_simulation_count()
-            show_full_output = self.get_show_full_output()
-            
-            win_count = 0
-            round_total = 0
-            for i in range(1, simulation_count + 1):
-                if show_full_output:
-                    print(f"\nGame {i}")
-                round_won = self.play_random(show_full_output)
-                if round_won > 0:
-                    round_total += round_won
-                    win_count += 1
-            
-            game_string = "game" if simulation_count == 1 else "games"
-            win_string = 'game' if win_count == 1 else 'games'
+        elif mode == 2:
+            self.handle_random_mode()
 
-            print(f"\n{simulation_count} random {game_string}")
-            print(f"Win percentage: {'{:.2%}'.format(win_count / simulation_count)} ({win_count} {win_string} won)")
-            
-            if win_count > 0:
-                print(f"Average round of won {win_string}: {'{:.2}'.format(round_total / win_count)}")
+        elif mode == 3:
+            self.handle_ai_mode()
 
-            print()
-
-
-
-        elif(mode == 3):
-            return
+    def handle_random_mode(self):
         
+
+        simulation_count = self.get_simulation_count()
+        show_full_output = self.get_show_full_output()
+
+
+        print("\nStarting random simulation...")
+        time.sleep(1.5)
+
+        win_count = 0
+        round_total = 0
+        for i in range(1, simulation_count + 1):
+            if show_full_output:
+                print(f"\nGame {i}")
+            round_won = self.play_random(show_full_output)
+            if round_won > 0:
+                round_total += round_won
+                win_count += 1
+
+        game_string = "game" if simulation_count == 1 else "games"
+        win_string = "game" if win_count == 1 else "games"
+
+        print(f"\n{simulation_count} random {game_string}")
+        print(
+            f"Win percentage: {'{:.2%}'.format(win_count / simulation_count)} ({win_count} {win_string} won)"
+        )
+
+        if win_count > 0:
+            print(
+                f"Average round of won {win_string}: {'{:.2}'.format(round_total / win_count)}"
+            )
+
+        print()
+
+    def handle_ai_mode(self):        
+        simulation_count = self.get_simulation_count()
+        show_full_output = self.get_show_full_output()
+
+
+        print("\nStarting AI simulation...")
+        time.sleep(1.5)
+
+        win_count = 0
+        round_total = 0
+        for i in range(1, simulation_count + 1):
+            if show_full_output:
+                print(f"\nGame {i}")
+            round_won = self.play_ai(show_full_output)
+            if round_won > 0:
+                round_total += round_won
+                win_count += 1
+
+        game_string = "game" if simulation_count == 1 else "games"
+        win_string = "game" if win_count == 1 else "games"
+
+        print(f"\n{simulation_count} AI {game_string}")
+        print(
+            f"Win percentage: {'{:.2%}'.format(win_count / simulation_count)} ({win_count} {win_string} won)"
+        )
+
+        if win_count > 0:
+            print(
+                f"Average round of won {win_string}: {'{:.2}'.format(round_total / win_count)}"
+            )
+
+        print()
 
     def get_simulation_count(self):
         while True:
             count = int(input("\tNumber of games: "))
-            if 0 < count <= 1000000:
+            if 0 < count <= 100000:
                 return count
-    
+
     def get_show_full_output(self):
         while True:
             show = input("\tShow full output? y/n: ")
-            if show == 'n':
+            if show == "n":
                 return False
-            elif show == 'y':
+            elif show == "y":
                 return True
+
+    def play_ai(self, show_full_output):
+        ai = ai_player.AIPlayer()
+
+        self.all_responses = list()
+        self.answer = self.get_random_word()
+        self.answer_char_counts = {
+            char: self.answer.count(char) for char in self.answer
+        }
+
+        for i in range(1, self.n_rounds + 1):
+            if show_full_output:
+                print(f"\nRound {i}")
+            while True:
+                user_guess = ai.get_next_guess()
+                if show_full_output:
+                    print(f"Guess: {user_guess}")
+                if user_guess in self.answers or user_guess in self.guessable:
+                    break
+
+            response = self.evaluate(user_guess)
+
+            if show_full_output:
+                print(f"Response: {response}")
+            if user_guess == self.answer:
+                self.display_results()
+                time.sleep(1.5)
+                return i
+
+        if show_full_output:
+            print(f"The word was {self.answer}\n")
+
+        self.display_results()
+
+        return 0
+
 
 wordle = Wordle()
 wordle.start()
-
 
 
 """
